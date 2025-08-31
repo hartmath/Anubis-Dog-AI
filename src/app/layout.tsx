2f4b8096192f38171313d99f175c5a248dae5ddd
Sofia Brown <sofiabrown1003@gmail.com>
2025-08-31 21:46:12 +0000

    Change the font to more techy one

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=PT+Sans:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Orbitron:wght@700&family=Roboto+Mono:wght@400;700&display=swap"
           rel="stylesheet"
         />
       </head>
